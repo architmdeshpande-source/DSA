7bb80a37a999bfe3d7fafbc401eea3f67ad3b6ad
architmdeshpande-source <architmdeshpande@gmail.com>
2026-09-23 17:41:38 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (13.54%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -12,32 +12,31 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        map<int,int> m;
-        queue<pair<TreeNode*, int>> q;
+        map<int, int> m;
         vector<int> ans;
         if(root == NULL) return ans;
-
-        q.push(make_pair(root,0));
+        queue<pair<TreeNode*, int>> q;
+        q.push(make_pair(root, 0));
 
         while(!q.empty()){
-            pair<TreeNode*, int> temp = q.front();
+            pair<TreeNode*, int> t = q.front();
+            TreeNode* frontNode = t.first;
             q.pop();
-
-            TreeNode* frontNode = temp.first;
-            int lvl = temp.second;
+            int lvl = t.second;
             m[lvl] = frontNode->val;
 
             if(frontNode->left){
-                q.push(make_pair(frontNode->left, lvl+1));
+                q.push(make_pair(frontNode->left, lvl + 1));
             }
             if(frontNode->right){
-                q.push(make_pair(frontNode->right, lvl+1));
+                q.push(make_pair(frontNode->right, lvl + 1));
             }
         }
 
         for(auto i : m){
             ans.push_back(i.second);
         }
+
         return ans;
     }
 };
