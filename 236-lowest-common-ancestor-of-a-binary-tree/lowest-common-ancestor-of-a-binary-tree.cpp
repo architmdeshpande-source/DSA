43044034c3cbd726e519e2b508d969549bec2901
architmdeshpande-source <architmdeshpande@gmail.com>
2026-09-23 19:54:24 +0530

    Time: 61 ms (70.70%) | Memory: 44 MB (25.99%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,15 +10,15 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == NULL) return root;
+        if(root == NULL) return NULL;
         if(root == p || root == q) return root;
 
         TreeNode* leftAns = lowestCommonAncestor(root->left, p, q);
         TreeNode* rightAns = lowestCommonAncestor(root->right, p, q);
 
-        if(leftAns!= NULL && rightAns!= NULL) return root;
-        else if(leftAns == NULL && rightAns!= NULL) return rightAns;
-        else if(leftAns!= NULL && rightAns == NULL) return leftAns;
+        if(leftAns != NULL && rightAns != NULL) return root;
+        else if(leftAns != NULL && rightAns == NULL) return leftAns;
+        else if(leftAns == NULL && rightAns != NULL) return rightAns;
         else return NULL;
     }
 };
